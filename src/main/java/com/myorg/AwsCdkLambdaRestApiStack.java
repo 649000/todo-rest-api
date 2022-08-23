fb85264920b33d84c5251ea06e25b204e0ea02b5
Nazri <649000@users.noreply.github.com>
2022-08-23 17:11:23 +0800

    Added dyanamoDB resource

--- src/main/java/com/myorg/AwsCdkLambdaRestApiStack.java
+++ src/main/java/com/myorg/AwsCdkLambdaRestApiStack.java
@@ -1,10 +1,13 @@
 package com.myorg;
 
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.services.dynamodb.Attribute;
+import software.amazon.awscdk.services.dynamodb.AttributeType;
+import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.TableProps;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
 
 public class AwsCdkLambdaRestApiStack extends Stack {
     public AwsCdkLambdaRestApiStack(final Construct scope, final String id) {
@@ -20,5 +23,22 @@ public class AwsCdkLambdaRestApiStack extends Stack {
         // final Queue queue = Queue.Builder.create(this, "AwsCdkLambdaRestApiQueue")
         //         .visibilityTimeout(Duration.seconds(300))
         //         .build();
+
+
+        TableProps tableProps;
+        Attribute partitionKey = Attribute.builder()
+                .name("itemId")
+                .type(AttributeType.STRING)
+                .build();
+        tableProps = TableProps.builder()
+                .tableName("items")
+                .partitionKey(partitionKey)
+                // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+                // the new table, and it will remain in your account until manually deleted. By setting the policy to
+                // DESTROY, cdk destroy will delete the table (even if it has data in it)
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
+        Table dynamodbTable = new Table(this, "items", tableProps);
+
     }
 }
