c07fcd1ea564a164d48b2b31fedbbc4283c3ee26
Nazri <649000@users.noreply.github.com>
2022-09-13 22:34:23 +0800

    Added skeletal files for lambda

--- src/main/java/com/myorg/AwsCdkLambdaRestApiApp.java
+++ src/main/java/com/myorg/AwsCdkLambdaRestApiApp.java
@@ -4,8 +4,6 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
-
 public class AwsCdkLambdaRestApiApp {
     public static void main(final String[] args) {
         App app = new App();
